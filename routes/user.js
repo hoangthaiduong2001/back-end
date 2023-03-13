c62200299b807a4e5481edae8ea6ba3e48816758
khanhdeptrai <tqk792001@gmail.com>
2023-03-13 10:30:05 +0700

    test deploy

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,15 @@ const {
 const router = require("express").Router();
 //GET ALL USERS
 router.get("/", verifyToken, userController.getAllUsers);
+router.get("/test", (req, res, next) => {
+  res.status(200).json({ success: "Hello Server" });
+});
 
 //DELETE USER
-router.delete("/:id", verifyTokenAndUserAuthorization, userController.deleteUser);
+router.delete(
+  "/:id",
+  verifyTokenAndUserAuthorization,
+  userController.deleteUser
+);
 
 module.exports = router;
